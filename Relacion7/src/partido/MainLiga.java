dd84baf18866e92a0d4e41bbdc1fe7f2e5227a41
Marta <Marta@host.docker.internal>
2021-01-20 21:39:43 +0100

    Version 4

--- Relacion7/src/partido/MainLiga.java
+++ Relacion7/src/partido/MainLiga.java
@@ -1,13 +1,74 @@
 package partido;
 
+import java.util.Scanner;
+
 public class MainLiga {
-
+	private static Scanner teclado = new Scanner(System.in);
 	public static void main(String[] args) throws LigaException {
-		Equipo equipoLocal = new Equipo("Sevilla", "Sanche Pijuan", "Sevilla");
-		Equipo equipoVisit = new Equipo("Real Madrid", "Santigo Bernabeu", "Madrid");
-		Partido partido1 = new Partido(1, equipoLocal, equipoVisit);
-		partido1.ponerResultado("1-2");
-		System.out.println(partido1);
-	}
+		String equipo = null, estadio = null, ciudad = null;
+		Equipo equipoLocal, equipoLocal2 = null, equipoLocal3 = null;
+		Equipo equipoVisit, equipoVisit2 = null, equipoVisit3 = null;
+		Partido partido1, partido2 = null, partido3 = null;
+		
+		// Aparecera primero este porque no hemos tenido que meter ningun dato.
+		// Hacemos el try-catch por si hubiese algun error que nos salte.
+		try {
+			
+			equipoLocal = new Equipo("Real Madrid", "Santigo Bernabeu", "Madrid");
+			equipoVisit = new Equipo("Betis", "Benito Villamarin", "Sevilla");
+			partido1 = new Partido(10, equipoLocal, equipoVisit);
+			
+			partido1.ponerResultado("2-1");
+			System.out.println(partido1);
+		}catch (LigaException e) {
+			System.out.println(e.getMessage());
+		}
 
+		// He decidido meter por teclado el nombre del equipo, estadio y ciudad.
+		try {
+			equiposFutbol();
+			equipoLocal2 = new Equipo(equipo, estadio, ciudad);
+			
+			equiposFutbol();
+			equipoVisit2 = new Equipo(equipo, estadio, ciudad);
+			partido2 = new Partido(11, equipoLocal2, equipoVisit2);
+			partido2.ponerResultado("0-0");
+			
+			
+			equiposFutbol();
+			equipoLocal3 = new Equipo(equipo, estadio, ciudad);
+			
+			equiposFutbol();
+			equipoVisit3 = new Equipo(equipo, estadio, ciudad);
+			partido3 = new Partido(12, equipoLocal3, equipoVisit3);
+			partido3.ponerResultado("1-2");
+			
+		}catch (LigaException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		// Saco por pantalla los quipos del partido 2 y del partido 3, tanto como la informacion de los partidos 2 y 3
+		System.out.println("---Partido 2---");
+		System.out.println(equipoLocal2);
+		System.out.println(equipoVisit2);
+		System.out.println(partido2);
+		System.out.println("---Partido 3---");
+		System.out.println(equipoLocal3);
+		System.out.println(equipoVisit3);
+		System.out.println(partido3);
+		
+	}
+	public static void equiposFutbol() {
+		String equipo, estadio, ciudad;
+		
+		System.out.println("Introduce el nombre del equipo:");
+		equipo = teclado.nextLine();
+		
+		System.out.println("Introduce el estadio del equipo:");
+		estadio = teclado.nextLine();
+		
+		System.out.println("Introduce la ciudad del quipo:");
+		ciudad = teclado.nextLine();
+	}
+	
 }
--- Relacion7/src/partido/Partido.java
+++ Relacion7/src/partido/Partido.java
@@ -5,8 +5,6 @@ public class Partido {
 	private int jornada;
 	private Equipo equipoLocal;
 	private Equipo equipoVisit;
-	private Equipo estadio;
-	private Equipo ciudad;
 	private int golesLocal;
 	private int golesVisit;
 	private char resultadoQuiniela;
@@ -18,9 +16,9 @@ public class Partido {
 		if (jornada > 38 || jornada < 1) {
 			throw new LigaException("Esa jornada no existe " + jornada + ". Solo hay jornadas del 1 al 38");
 		}
-		this.jornada = jornada;
-		this.equipoLocal = equipoLocal;
-		this.equipoVisit = equipoVisit;
+		setJornada(jornada);
+		setEquipoLocal(equipoLocal);
+		setEquipoVisit(equipoVisit);
 		jugado = false;
 	}
 	
@@ -32,12 +30,33 @@ public class Partido {
 	public void setJornada(int jornada) {
 		this.jornada = jornada;
 	}
+	
+	public Equipo getEquipoLocal() {
+		return equipoLocal;
+	}
+	
+	public Equipo setEquipoLocal(Equipo equipoLocal) {
+		this.equipoLocal = equipoLocal;
+		return equipoLocal;
+	}
+	
+	public Equipo getEquipoVisit() {
+		return equipoVisit;
+	}
+	
+	public Equipo setEquipoVisit(Equipo equipoVisit) {
+		this.equipoVisit = equipoVisit;
+		return equipoVisit;
+	}
 
 	public int getGolesLocal() {
 		return golesLocal;
 	}
 
-	public void setGolesLocal(int golesLocal) {
+	public void setGolesLocal(int golesLocal) throws LigaException {
+		if(golesLocal < 0) {
+			throw new LigaException("Los goles no pueden ser negativos");
+		}
 		this.golesLocal = golesLocal;
 	}
 
@@ -45,7 +64,10 @@ public class Partido {
 		return golesVisit;
 	}
 
-	public void setGolesVisit(int golesVisit) {
+	public void setGolesVisit(int golesVisit) throws LigaException {
+		if(golesVisit < 0) {
+			throw new LigaException("Los goles no pueden ser negativos");
+		}
 		this.golesVisit = golesVisit;
 	}
 
@@ -62,9 +84,9 @@ public class Partido {
 		String info;
 		
 		if (jugado == true) {
-			info = "Partido entre equipo local " + equipoLocal + " y equipo visitante " + equipoVisit + " jugado en el estadio " + estadio + 
-					" de la ciudad " + ciudad + " ha finalizado con " + golesLocal + " goles del equipo local y " + golesVisit + 
-					" goles del equipo visitante. Resultado quiniela = " + resultadoQuiniela;
+			info = "Partido entre equipo local " + this.equipoLocal.getNombreEquipo() + " y equipo visitante " + this.equipoVisit.getNombreEquipo() + " jugado en el estadio " + this.equipoLocal.getNombreEstadio() + 
+					" de la ciudad " + this.equipoLocal.getCiudad() + " ha finalizado con " + this.golesLocal + " goles del equipo local y " + this.golesVisit + 
+					" goles del equipo visitante. Resultado quiniela = " + this.resultadoQuiniela;
 		}else {
 			info = "Partido entre equipo local " + equipoLocal + " y equipo visitante " + equipoVisit + " todavía no se ha jugado";
 		}
@@ -73,28 +95,24 @@ public class Partido {
 	}
 	
 	public void ponerResultado(String resultado) throws LigaException{
-		int result1,result2;
+		
 		String [] parts = resultado.split("-");
 		// Este sería los goles del equipo local. Ej: 1-2. Sacaría el numero 1.
 		String part1 = parts[0]; 
-		result1 = Integer.parseInt(part1);
+		golesLocal = Integer.parseInt(part1);
 		// Este sería los goles del equipo visitante.Ej: 1-2. Sacaría el numero 2.
 		String part2 = parts[1];
-		result2 = Integer.parseInt(part2);
-		if(result1 > result2) {
+		golesVisit = Integer.parseInt(part2);
+		if(golesLocal > golesVisit) {
 			resultadoQuiniela = '1';
 		}else {
-			if (result1 < result2) {
+			if (golesLocal < golesVisit) {
 				resultadoQuiniela = '2';
 			}else {
-				if (result1 == result2) {
+				if (golesLocal == golesVisit) {
 					resultadoQuiniela = 'X';
 				}
 			}
-		}
-		// EXCEPTION
-		if (result1 < 0 || result2 < 0) {
-			throw new LigaException("Los goles no pueden ser negativos");
 		}
 		jugado = true;
 	}
